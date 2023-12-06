5bf5e55e8f25fa19bc4029d1156e719bee0901d1
Annevd <144004647+Annevd@users.noreply.github.com>
2023-12-06 13:11:07 +0100

    v2 UI

--- scripts/script.js
+++ scripts/script.js
@@ -45,3 +45,38 @@ modeSwitch.addEventListener("click" , () =>{
         
     }
 });
+
+
+// Popup warning
+
+const showButton = document.getElementById("button");
+const dialog = document.querySelector("dialog");
+const closeButton = document.getElementById("popup-close");
+const deleteButton = document.getElementById("popup-delete");
+const dialogSuccess = document.getElementById("dialog-success");
+const closeButtonSuccess = document.getElementById("success-close");
+
+
+// Openen waarchuwing popup
+showButton.addEventListener("click", () => {
+  dialog.showModal(); //opent de poup als je op de prullenbak klikt
+  document.querySelector("body").classList.add("dont-scroll"); //toggled de class dat ervoor zorgt dat je in de popup niet kan scrollen
+});
+
+// Sluit de waarschuwing popup als gebruiker op nee klikt
+closeButton.addEventListener("click", () => {
+  dialog.close();
+  document.querySelector("body").classList.remove("dont-scroll"); // verwijderd de classlist zodat je weer kan scrollen
+});
+
+// Opent succesvolle melding als gebruiker op ja klikt
+deleteButton.addEventListener("click", () => {
+  dialog.close();
+  dialogSuccess.showModal();
+} )
+
+// Sluit de succes melding als de gebruiker op het kruisje klikt
+closeButtonSuccess.addEventListener("click", () => {
+  dialogSuccess.close();
+  document.querySelector("body").classList.remove("dont-scroll"); // verwijderd de classlist zodat je weer kan scrollen
+})
